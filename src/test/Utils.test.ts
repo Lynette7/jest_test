a0fd5a012068716107be1bf8c23dcfc74b9059b1
Lynette7 <lynettewanjiru7@gmail.com>
2023-08-02 21:43:46 +0300

    feat: unit test structure

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -3,8 +3,15 @@ import { toUpperCase } from "../app/Utils"
 
 describe('Utils test suite', () => {
 
-    test('should return uppercase', ()=>{
-        const result = toUpperCase('abc');
-        expect(result).toBe('ABC');
+    it('should return uppercase of valid string', ()=>{
+        //arrange:
+        const sut = toUpperCase;
+        const expected = 'ABC';
+
+        //act:
+        const actual = sut('abc');
+
+        //assert:
+        expect(actual).toBe(expected);
     })
 })
