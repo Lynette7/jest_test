2c4a394971b6481210804e7f2d90ace436eec0d8
Lynette7 <lynettewanjiru7@gmail.com>
2023-08-10 22:23:25 +0300

    chore:password checker iteration 1

--- src/test/pass_checker/PasswordChecker.test.ts
+++ src/test/pass_checker/PasswordChecker.test.ts
@@ -8,9 +8,34 @@ describe('PasswordChecker test suite', ()=>{
         sut = new PasswordChecker();
     })
 
-    it('Should do nothing for the moment', ()=>{
-        sut.checkPassword();
+    it('Password with less than 8 characters is invalid', ()=>{
+        const actual = sut.checkPassword('1234567');
+        expect(actual).toBe(false);
     })
 
+    it('Password with 8 characters or more is valid', ()=>{
+        const actual = sut.checkPassword('12345678Aa');
+        expect(actual).toBe(true);
+    })
+
+    it('Password with no upper case letter is invalid', ()=>{
+        const actual = sut.checkPassword('1234abcd');
+        expect(actual).toBe(false);
+    })
+
+    it('Password with an upper case letter is okay', ()=>{
+        const actual = sut.checkPassword('1234abcdA');
+        expect(actual).toBe(true);
+    })
+
+    it('Password with no lower case letter is invalid', ()=>{
+        const actual = sut.checkPassword('1234ABCD');
+        expect(actual).toBe(false);
+    })
+
+    it('Password with a lower case letter is okay', ()=>{
+        const actual = sut.checkPassword('1234ABCDa');
+        expect(actual).toBe(true);
+    })
 
 })
